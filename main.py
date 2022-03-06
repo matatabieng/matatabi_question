c7bda08970f69371bd9fbfb42a5f44ed7c84f0e4
matatabieng <101035019+matatabieng@users.noreply.github.com>
2022-03-06 15:42:24 +0900

    改善（５）

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import tweepy
 CK = "sHdXaO0oNTDh7KK3OQCzhw75N"
 CS = "hceggx0eFZ7xWV4TfPwfCo5rFBOIk9kv3dH0UUzGLhNIBGSlY9"
 
+AK="1500064603836870659-y3vFNvAt0nlQO2GKfvdTmTfElGKyyE"
+AS="i0WwHsm7CRbWXJIeQ3uAdnbJ4BthSjoKToFGSFERC6Rvv"
+
+
 # Twitterオブジェクトの生成
 auth = tweepy.OAuthHandler(CK, CS)
-auth.set_access_token(auth.access_token, auth.access_token_secret)
+auth.set_access_token(AK, AS)
 
 api = tweepy.API(auth)
 
