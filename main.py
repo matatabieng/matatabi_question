93e707e197053fcdec1169bed4a95e6135d85de6
matatabieng <101035019+matatabieng@users.noreply.github.com>
2022-03-07 21:18:07 +0900

    万葉集
    #またたび万葉集

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ auth.set_access_token(AK, AS)
 api = tweepy.API(auth)
 
 # 1ツイートずつループ。
-for status in api.search(q='#またたび質問箱英語', count=50):
+for status in api.search(q='#またたび万葉集', count=50):
     tweet_id = status.id
     # 例外処理をする
     try:
