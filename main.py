670611005e5f821e1248e130687ff352c6d9d258
matatabieng <101035019+matatabieng@users.noreply.github.com>
2022-04-01 14:08:50 +0900

    環境変数

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import tweepy
 import os
 
-# CK = "sHdXaO0oNTDh7KK3OQCzhw75N"
-# CS = "hceggx0eFZ7xWV4TfPwfCo5rFBOIk9kv3dH0UUzGLhNIBGSlY9"
-
-# AK="1500064603836870659-y3vFNvAt0nlQO2GKfvdTmTfElGKyyE"
-# AS="i0WwHsm7CRbWXJIeQ3uAdnbJ4BthSjoKToFGSFERC6Rvv"
 
 
 CK=os.environ.get("CK")
